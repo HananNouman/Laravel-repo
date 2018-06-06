56856932e09840b06ff65caa4ac6b62e221a1fcf
HananNouman <hnouman@hotmail.co.uk>
2018-06-06 22:43:16 +0300

    laravel

---
+++ config/laravel-owm.php
@@ -0,0 +1,6 @@
+<?php
+
+return [
+    'api_key' => '9c2b064b49f0eaf8b682078f9c920635',            // visit: http://openweathermap.org/appid#get for more info.
+    'routes_enabled' => true,   // If the routes have to be enabled.
+];
